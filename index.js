d31393801f76341e634f02a4bbdbd01ef06b4d11
dena8 <Perindopril88@>
2021-06-09 09:37:41 +0300

    Creating project structure and initial setup on Express.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Travel Agency App!')
-})
+require('./config/express')(app);
 
-app.listen(port, () => {
-  console.log(`Travel agency app listening at http://localhost:${port}`)
-})
+
+app.listen(port,(err)=>{
+  if(err){
+      throw new Error('error application')
+  }
+  console.log(`Travel agency app listening at http://localhost:${port}`);
+});
